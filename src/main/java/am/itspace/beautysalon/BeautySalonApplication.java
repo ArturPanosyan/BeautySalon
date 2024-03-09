ae29ebeccd9e0448a3afd2cc24936279dda64b36
Mr_Artur <panosyanartur31@gmail.com>
2024-03-09 16:07:22 +0400

    Beauty

--- src/main/java/am/itspace/beautysalon/BeautySalonApplication.java
+++ src/main/java/am/itspace/beautysalon/BeautySalonApplication.java
@@ -2,8 +2,15 @@ package am.itspace.beautysalon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class BeautySalonApplication {
 
     public static void main(String[] args) {
@@ -11,4 +18,8 @@ public class BeautySalonApplication {
     }
 
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
