cc81469d3765f233a963205ecbcf17017c6c8468
Mr_Artur <panosyanartur31@gmail.com>
2024-03-12 00:42:35 +0400

    Beauty

--- src/main/java/am/itspace/beautysalon/BeautySalonApplication.java
+++ src/main/java/am/itspace/beautysalon/BeautySalonApplication.java
@@ -2,11 +2,9 @@ package am.itspace.beautysalon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 @EnableAsync
@@ -18,8 +16,4 @@ public class BeautySalonApplication {
     }
 
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
---
+++ src/main/java/am/itspace/beautysalon/api/MainApi.java
@@ -0,0 +1,13 @@
+package am.itspace.beautysalon.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainApi {
+    @GetMapping("/")
+    public String mainApi(){
+        return "index";
+    }
+
+}
